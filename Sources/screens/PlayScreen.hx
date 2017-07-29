a8bf7181196ac254bbfed5d3f7c9dead44178d19
John Doughty <jrdoughty13@gmail.com>
2017-07-28 23:56:59 -0500

    put characters at bottom

--- Sources/screens/PlayScreen.hx
+++ Sources/screens/PlayScreen.hx
@@ -1,11 +1,9 @@
 package screens;
 
 import kha.Assets;
+import sdg.graphics.shapes.Polygon;
 import sdg.Screen;
 import sdg.Object;
-import sdg.graphics.tiles.Tilemap;
-import sdg.graphics.tiles.Tileset;
-import sdg.components.Component;
 
 class PlayScreen extends Screen
 {
@@ -16,6 +14,11 @@ class PlayScreen extends Screen
 	public override function init()
 	{
 		super.init();
+		var char = new Object(16,200,Polygon.createRectangle(16,32,kha.Color.Green,true,.2));
+		add(char);
+
+		var bull = new Object(200,200,Polygon.createRectangle(64,32,kha.Color.Red,true,.2));
+		add(bull);
 	}
 
 	public override function update()
